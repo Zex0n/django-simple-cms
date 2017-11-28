166bf2bdb3630b76d46f608cbed6ae9a66889b34
Rock <ivan.tolkachev@gmail.com>
2017-11-28 13:53:00 +0300

    last version

--- shop/templatetags/cms_tags.py
+++ shop/templatetags/cms_tags.py
@@ -6,6 +6,19 @@ def add_class(field, css):
    return field.as_widget(attrs={"class":css})
 
 
+@register.simple_tag(takes_context = True)
+def cookie(context, cookie_name): # could feed in additional argument to use as default value
+    request = context['request']
+    result = request.COOKIES.get('t'+str(cookie_name),'') # I use blank as default value
+
+
+    if result=='':
+        result='0'
+
+
+    return result
+
+
 @register.simple_tag()
 def mult(value, arg):
 
