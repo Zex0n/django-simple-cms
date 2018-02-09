782c2478bb87b402e3383c166c5767b935325a51
denis <trumpk@gmail.com>
2018-02-09 17:13:16 +0300

    backupdata

---
+++ pgdata/globals.sql
@@ -0,0 +1,25 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+
+
+
+
+
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
