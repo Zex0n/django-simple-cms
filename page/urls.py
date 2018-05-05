ce906631322353c2f0d5e8a71a45b4c1c8f0d8b3
denis <trumpk@gmail.com>
2018-05-05 19:56:50 +0300

    Capcha в форму отправки

--- page/urls.py
+++ page/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.DetailView.as_view(), {'slug': 'main'}, name='index'),
 
     url(r'^page/goodpost/?$', TemplateView.as_view(template_name="page/good_post.html")),
+    url(r'^page/errorpost/?$', TemplateView.as_view(template_name="page/error_post.html")),
     url(r'^page/(?P<slug>[-\w]+)/$', views.DetailView.as_view(), name='pagedetail'),
     url(r'^page/sendmymail/?$', views.SendMailCls.as_view(), name='sendmymail'),
 
